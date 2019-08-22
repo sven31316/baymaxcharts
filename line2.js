cce93fabdfd3c05d14e78dd5302a4a2ea09b04ce
yzqdev <yzqdev@outlook.com>
2019-08-22 23:08:25 +0800

    init

---
+++ line2.js
@@ -0,0 +1,18 @@
+var line2 = echarts.init(document.getElementById("line2"));
+var option = {
+
+    tooltip: {},
+
+    xAxis: {
+        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+    },
+    yAxis: {},
+    series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+    }]
+};
+
+
+line2.setOption(option);
